c09227cdc3f67379517fb258d2d116c8dadaa594
yifeng <493788939lizhaogai@163.com>
2018-10-18 16:18:58 +0800

    no message

--- exmaples/client.js
+++ exmaples/client.js
@@ -2,12 +2,14 @@ const Client = require('../src').RPCClient;
 
 async function t() {
     let client = new Client('amqp://nevem:nevem@localhost/order');
+    let i = 0;
     setInterval(async function () {
         let result = await client.call('add', 1, 2).catch(e => {
             return e;
         });
-        console.log(result);
-    }, 1000);
+        i++;
+        console.log(result + ':' + i);
+    }, 10);
 }
 
 t();
