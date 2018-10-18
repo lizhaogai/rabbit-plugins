7c4db10838d28bdb7f7c06de59aa8f844edf7d45
yifeng <493788939lizhaogai@163.com>
2018-10-18 20:27:05 +0800

    no message

--- examples/client.js
+++ examples/client.js
@@ -3,13 +3,16 @@ const Client = require('../src').RPCClient;
 let client = new Client('amqp://nevem:nevem@localhost/order');
 
 let i = 0;
-setInterval(async function () {
-    i++;
-    client.call('add', 1, 2, i).catch(e => {
-        return e;
-    }).then(result => {
-        console.log(JSON.stringify(result) + ':' + i);
-    })
+client.$promise.then(() => {
+    setInterval(async function () {
+        i++;
+        client.call('add', 1, 2, i).catch(e => {
+            return e;
+        }).then(result => {
+            console.log(JSON.stringify(result) + ':' + i);
+        })
 
 
-}, 1);
+    }, 1);
+});
+
--- examples/server.js
+++ examples/server.js
@@ -1,7 +1,10 @@
 const Server = require('../src').RPCServer;
 
 let server = new Server('amqp://nevem:nevem@localhost/order');
-server.method('add', (a, b, i) => {
-    console.log(i);
-    return a + b;
+server.$promise.then(() => {
+    server.method('add', (a, b, i) => {
+        console.log(i);
+        return a + b;
+    });
 });
+
